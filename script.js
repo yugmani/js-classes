4f82417bd411bbcb6a8fa914eba2d1db4abd7c06
yugmani <yugmani@yahoo.com>
2021-06-12 23:54:06 -0700

    set cookies

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello!')
+function writeCookie() {
+  if(document.myForm.customer.value == ""){
+    alert("Enter the value!");
+    return;
+  }
+
+  cookievalue = escape(document.myForm.customer.value) + ";";
+  document.cookie = "name=" + cookievalue;
+  alert("Setting Cookies: " + "name=" + cookievalue);  
+}
